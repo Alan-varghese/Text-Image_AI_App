42b44263fc71d831d8a6e4de19b96030968e91a4
Alan Varghese <alanvz757@gmail.com>
2025-01-17 00:47:38 +0530

    Updated

--- lib/feature/prompt/bloc/prompt_repo.dart
+++ lib/feature/prompt/bloc/prompt_repo.dart
@@ -2,10 +2,8 @@ class PromptRepo {
   static generateImage(String prompt) async {
     String url = 'POST https://api.vyro.ai/v2/image/generations';
     Map<String, dynamic> headers = {
-      'Authorization':'Authorization: Bearer {{API_TOKEN}}'
-
+      'Authorization':
+          'Authorization: Bearer vk-30cri21jf2ygL0ggs4RNCF8azw1xlkmRzAOYz90Dk30BjWb'
     };
-
-
   }
 }
