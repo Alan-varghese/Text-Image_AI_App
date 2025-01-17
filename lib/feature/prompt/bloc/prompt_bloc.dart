d90587699bd71035e0f02c1c1cf77bed9236c801
Alan Varghese <alanvz757@gmail.com>
2025-01-17 18:31:45 +0530

    Updated

--- lib/feature/prompt/bloc/prompt_bloc.dart
+++ lib/feature/prompt/bloc/prompt_bloc.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:ai_image_app/feature/prompt/bloc/prompt_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -11,6 +13,14 @@ class PromptBloc extends Bloc<PromptEvent, PromptState> {
     on<PromptEnteredEvent>(_onPromptEnteredEvent);
   }
 
-  Future<void> _onPromptEnteredEvent(PromptEnteredEvent event, Emitter<PromptState> emit) async {
+  Future<void> _onPromptEnteredEvent(
+      PromptEnteredEvent event, Emitter<PromptState> emit) async {
+    emit(PromptGeneratingImageLoadState());
+    File? file = await PromptRepo.generateImage(event.prompt);
+    if (file != null) {
+      emit(PromptGeneratingImageSuccessState());
+    } else {
+      emit(PromptGeneratingImageErrorState());
+    }
   }
 }
